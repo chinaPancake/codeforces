8228bb3142de91939a32ba7cdf962a9557ae0963
ChinaPancake <mateusz.placek99@gmail.com>
2022-12-15 21:08:21 +0100

    day2

---
+++ challenging_valleys.py
@@ -0,0 +1,51 @@
+def state_f(arr):
+    ans = 'dol'
+    for l,n in zip(arr,arr[1:]):
+        if ans == 'dol':
+            if l<n:
+                ans = 'gora'
+
+        elif ans =='gora':
+            if l>n:
+                ans = 'dol'
+                return 'NO'
+        
+    return 'YES'
+
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    yn = 'YES'
+    print(state_f(arr=arr))   
+       
+
+
+# T -> F -> bład
+# .   .
+#  . .
+#   . 
+#
+#[3,2,1,2,3,] 
+#
+# T-> F -> BŁAD
+# [1,2,3,4,3,2,1]
+#           .
+#       .       .
+#   .               .
+#.                      .
+#
+# [3,2,1 ,2,3 ,2,3,4 ]
+# T-> F->T->F - Bład
+#                           .
+#.              .       .
+#   .       .       .
+#       .               
+#
+#
+# T-> Good
+#                           .
+#                       .
+#               .   .   
+#           .   
+#.  .   .
