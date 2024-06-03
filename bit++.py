3909ec81a341ea42828838e81412de04daf5ee32
ChinaPancake <mateusz.placek99@gmail.com>
2024-06-03 17:13:06 +0200

    bit++

--- bit++.py
+++ bit++.py
@@ -7,11 +7,5 @@ for _ in range(int(input())):
         o+=1
     elif any(x in word for x in minus):
         o-=1
-
-    # if '++x' or 'x++' in word:
-    #     o+=1
-    #     print(word, o)
-    # elif '--x' or 'x--' in word:
-    #     o-=1
-    
+   
 print(o)
