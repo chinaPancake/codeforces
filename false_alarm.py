9a8bd53b24d24d2a9c8c88ef93d5c9591c2cda91
ChinaPancake <mateusz.placek99@gmail.com>
2025-06-09 00:03:51 +0200

    00:03 fixed wit while loop false_alarm.py)

--- false_alarm.py
+++ false_alarm.py
@@ -6,25 +6,14 @@ for i in range(int(input())):
     #print(door_list)
     first_door = int(0)
     last_door = int(0)
-    for first in door_list:
-#        print('pierwsze drzwi: ', first)
-#        print("tak wyglada lista: ", door_list)
-        if door_list[0] == "0":
-            first_door += 1
-            door_list.pop(0)
+    
+    while door_list and door_list [0] == "0":
+        first_door += 1
+        door_list.pop(0)
+    while door_list and door_list [-1] == "0":
+        first_door += 1
+        door_list.pop()
 
-        elif first == "1":
-            break
- #   print("po pierwszej petli", door_list)
-
-    for last in reversed(door_list):
-  #      print("osttanie drzwi: ", last)
-        if door_list[-1] == "0":
-            last_door += 1
-            door_list.pop()
-
-        elif last == "1":
-            break
 
    # print("po drugiej", door_list)
     #print('od poczatku: ', first_door, 'od konca: ', last_door, 'ile miedzy nimi: ', len(door_list), 'ile sekund: ', button)
